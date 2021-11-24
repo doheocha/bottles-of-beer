743823b47a1eb198fa4637c6105f671813a37b05
Dara O'hEocha <doheocha@hotmail.co.uk>
2021-11-24 21:01:31 +0000

    bottles of beer

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
+let i = 0;
 function printBottles() {
-    let i = 0;
-    while (i<100) {
-        let song = " bottles of beer on the wall"
-        document.getElementById("bottles").innerHTML = i + song;
+    while (i<10) {
+        setTimeout(function(){ printRealBottles();}, 3000);
+        i++;
+        break;
     }
+}
+
+function printRealBottles() {
+    document.getElementById("bottles").innerHTML = i;
+    setTimeout(2000);
 }
